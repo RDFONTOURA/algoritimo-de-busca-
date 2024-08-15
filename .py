e79b74ecdd4da6a3a92ea044682ed5e7467a7f2f
Ricardo Fontoura <pitifontoura55@gmail.com>
2024-08-14 21:31:28 -0300

    primeiro commit

---
+++ .py
@@ -0,0 +1,11 @@
+login = input("Digite o seu login:")
+senha = input("Digite sua senha:")
+if login == "nilce" and senha == "teste123":
+    print("Bem-vindo userpy01")
+else:
+    print("login falhou, verifique sua senha e tente novamente")
+
+
+
+
+
